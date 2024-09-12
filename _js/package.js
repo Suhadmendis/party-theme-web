0f0e9ee2773e63fc62fc61aa7b861c41351584cf
Suhad Mendis <Suhad.a.mendis@gmail.com>
2024-09-12 15:59:49 +0530

    changes

--- _js/package.js
+++ _js/package.js
@@ -1,7 +1,7 @@
 const rentalPackages = [
   {
     packageName: 'Premium Package',
-    imageUrl: 'images/package-image.png',
+    imageUrl: 'images/package_image.webp',
     amount: 100000,
     material: 'High-quality, wrinkle-resistant polyester fabric',
     description: 'Our top-tier package, perfect for weddings or large parties. Includes premium tents, elegant chairs, round tables, ambient lighting, a high-quality sound system, and stage setup for up to 200 guests.',
@@ -9,7 +9,7 @@ const rentalPackages = [
   },
   {
     packageName: 'Standard Package',
-    imageUrl: 'images/package-image.png',
+    imageUrl: 'images/package_image.webp',
     amount: 50000,
     material: 'High-quality, wrinkle-resistant polyester fabric',
     description: 'Designed for medium-sized gatherings like birthdays or family celebrations. Includes standard tents, chairs, rectangular tables, basic lighting, and a sound system for up to 100 guests.',
@@ -17,7 +17,7 @@ const rentalPackages = [
   },
   {
     packageName: 'Economy Package',
-    imageUrl: 'images/package-image.png',
+    imageUrl: 'images/package_image.webp',
     amount: 30000,
     material: 'High-quality, wrinkle-resistant polyester fabric',
     description: 'Ideal for smaller events like intimate parties or baby showers. Includes a basic tent, simple chairs and tables for up to 50 guests, with optional light setup.',
@@ -25,7 +25,7 @@ const rentalPackages = [
   },
   {
     packageName: 'Corporate Executive Package',
-    imageUrl: 'images/package-image.png',
+    imageUrl: 'images/package_image.webp',
     amount: 150000,
     material: 'High-quality, wrinkle-resistant polyester fabric',
     description: 'A comprehensive solution for corporate events such as conferences, product launches, or gala dinners. Includes large, modern tents, conference tables, executive chairs, a high-end audio-visual setup, and full lighting for up to 300 attendees.',
@@ -33,7 +33,7 @@ const rentalPackages = [
   },
   {
     packageName: 'Corporate Essentials Package',
-    imageUrl: 'images/package-image.png',
+    imageUrl: 'images/package_image.webp',
     amount: 80000,
     material: 'High-quality, wrinkle-resistant polyester fabric',
     description: 'Tailored for business meetings, workshops, or team-building events. Includes tents, standard conference tables, chairs, projectors, and basic sound systems for up to 150 attendees.',
