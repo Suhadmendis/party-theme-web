c56bb44b676172c7d1f6f0e9207bbfad5787d64b
Suhad Mendis <Suhad.a.mendis@gmail.com>
2024-10-24 20:39:12 +0530

    changes

--- config/db.php
+++ config/db.php
@@ -25,5 +25,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully\n";
+// echo "Connected successfully\n";
 ?>
