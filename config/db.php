8308b5ce4c22a3268cf62ff7f6db06db7c692100
Suhad Mendis <Suhad.a.mendis@gmail.com>
2024-10-24 20:36:25 +0530

    changes

--- config/db.php
+++ config/db.php
@@ -1,8 +1,22 @@
 <?php
-$servername = "localhost"; // Your database server
-$username = "root"; // Database username
-$password = "root"; // Database password
-$dbname = "party_theme_db";   // Database name
+$servername = ""; // Your database server
+$username = ""; // Database username
+$password = ""; // Database password
+$dbname = "";   // Database name
+
+if ($_SERVER['HTTP_HOST'] == 'localhost:8888') {
+    $servername = "localhost"; // Your database server
+    $username = "root"; // Database username
+    $password = "root"; // Database password
+    $dbname = "party_theme_db";   // Database name
+}
+if ($_SERVER['HTTP_HOST'] == 'partytheme.lk') {
+    $servername = "170.249.212.10"; // Your database server
+    $username = "partythe_akila"; // Database username
+    $password = "SaGaRa4000"; // Database password
+    $dbname = "partythe_me_db";   // Database name
+}
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,5 +25,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// echo "Connected successfully\n";
+echo "Connected successfully\n";
 ?>
